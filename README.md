788331cecc5e5fd31ba856b35081cdf0e68967ad
eleijonmarck <ericle@kth.se>
2016-07-04 10:12:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LIGOP - Life goal planner
+
+The lifegoal planner is a tool to visualize your coming and past goals in
+a holistic approach. You will be able to plan rough time estimates with
+the help of ligop and make it availble for everyone to see.
---
+++ src/main/java/ligop/Receiver.java
@@ -0,0 +1,26 @@
+package ligop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by ericleijonmarck on 2016-06-24.
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
